be7c8b134499bbfaecf81d6802216396ca5c6b07
Alek Westover <alek.westover@gmail.com>
2019-10-07 17:03:29 -0400

    inti

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# nhs
-nhs
+# This project is to generate a website for NHS
+# Instructions
+  * Go to the google doc with everyone's Bio in it 
+  * click File, download, download as website
+  * prettify the html https://htmlformatter.com/ (this is strictly speaking not necessary, but do it anyways)
+  * run the parsing code to get separate files for everyone.
+
+  * NOTE: it would be way better if people inputted the bio info via a google sheet, or if I had a list of all the names...
+
+  * you can (probably) get such a list by going to the name section of classroom and 
+    - copying the table
+    - putting it in http://www.convertcsv.com/html-table-to-csv.htm
+    - downloading a csv 
+    - warning: this tool is kinda slow
+  * now that I have this, it should be possible to make a much better parser 
+    - the one stipulation is that it means that I will have to force people to spell their names right in the document!!
+
+# Possible features
+  * tutor sign in
+  * a legit domain name
+  * sign up for tutoring
+  * use google api so that we are secretly using a google form instead of overtly using it
+  * style it up
+  * view bios
+  * search for people
+  * search for topics
+  * rate people 
+
---
+++ nhsBios/pageParser.py
@@ -0,0 +1,59 @@
+
+from bs4 import BeautifulSoup
+import json
+
+with open("names.json", "r") as f:
+    names = json.load(f)
+
+#  with open("NHSBios20182019.html", "r") as f:
+with open("formattedNHSBios.html", "r") as f:
+    data = f.read().strip() 
+data = BeautifulSoup(data, features="lxml")
+data = data.body
+all_tags = [tag for tag in data.children]
+partitioned_tags = [[]]
+page_break_tag= '<hr style="page-break-before:always;display:none;"/>'
+
+for tag in all_tags:
+    if tag.name != "hr":
+        partitioned_tags[-1].append(tag)
+    else:
+        partitioned_tags.append([])
+
+separate_people = []
+for part in partitioned_tags:
+    current_person = ""
+    for tag in part:
+        stag = str(tag)
+        if page_break_tag in stag:
+            stags = stag.split(page_break_tag)
+            current_person += stags[0]
+            current_person += stags[1]
+            separate_people.append(current_person)
+            current_person = ""
+        else:
+            current_person += stag
+    separate_people.append(current_person)
+
+total = ""
+for i in range(len(separate_people)):
+    curId = str(i)
+    for name in names:
+        if name in separate_people[i]: 
+            curId = name
+            curId = curId.replace(" ", "_")
+            break
+
+    total += f'<div style="display:none" class="row" id="{curId}">\n'
+    total += '<div class="col col-11">\n'
+    total += separate_people[i]
+    total += "</div>\n"
+    total += '<div class="col-1">\n'
+    total += f"<button>Schedule<br>Appointment<br>With<br>{curId.replace('_',' ')}</button> "
+    total += '</div>'
+    total += '\n</div>'
+    total += "\n\n\n\n\n"
+
+with open("split_bios.html", "w") as f:
+    f.write(total)
+
